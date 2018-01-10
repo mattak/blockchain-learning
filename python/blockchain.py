85d9c9f8280d55c66ed4e136720cba92e5c844a8
mattak <mattak.me@gmail.com>
2018-01-10 10:18:49 +0900

    create node api

--- python/blockchain.py
+++ python/blockchain.py
@@ -1,7 +1,11 @@
 import json
 import hashlib
 from time import time
+from urllib.parse import urlparse
 from uuid import uuid4
+
+import sys
+import requests
 from flask import Flask, jsonify, request
 
 
@@ -9,9 +13,23 @@ class Blockchain(object):
     def __init__(self):
         self.current_transactions = []
         self.chain = []
+        self.nodes = set()
 
         # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
+
+    def register_node(self, address):
+        """
+        Add a new node to the list of nodes
+
+        :param address: <str> Address of node. Eg. "http://192.168.0.5:5000"
+        :return: None
+        """
+
+        parsed_url = urlparse(address)
+
+        if parsed_url.netloc != "":
+            self.nodes.add(parsed_url.netloc)
 
     def new_block(self, proof, previous_hash=None):
         """
@@ -57,19 +75,7 @@ class Blockchain(object):
     def last_block(self):
         return self.chain[-1]
 
-    @staticmethod
-    def hash(block):
-        """
-        Creates a SHA-512 hash of a Block
-
-        :param block: <dict> Block
-        :return: <str> Hash of block
-        """
-
-        block_string = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha512(block_string).hexdigest()
-
-    def proof_of_work(self, last_proof, difficulty):
+    def proof_of_work(self, last_proof, difficulty=4):
         """
         Simple Proof of Work Algorithm
         - Find a number p' such that hash(pp') contains leading 4 zeros, where p is the previous p'
@@ -85,6 +91,81 @@ class Blockchain(object):
             proof += 1
 
         return proof
+
+    def valid_chain(self, chain):
+        """
+        Determine if a given blockchain is valid.
+
+        :param chain: <list> A blockchain
+        :return: <bool> True if valid, False if not
+        """
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n----------\n")
+
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        """
+        This is our Consensus Algorithm, it resolves conflicts
+        by replacing our chain with the longest one in the network.
+
+        :return: <bool> True if our chain was replaced, False if not
+        """
+
+        neighbours = self.nodes
+        new_chain = None
+
+        # We're only looking for chains longer than ours
+        max_length = len(self.chain)
+
+        # Grab and verify the chians from all the nodes in our networks
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # Check if the length is longer and the chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
+    @staticmethod
+    def hash(block):
+        """
+        Creates a SHA-512 hash of a Block
+
+        :param block: <dict> Block
+        :return: <str> Hash of block
+        """
+
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha512(block_string).hexdigest()
 
     @staticmethod
     def valid_proof(last_proof, proof, difficulty):
@@ -114,7 +195,7 @@ def mine():
     # We run the proof of work algorithm to get the next proof
     last_block = blockchain.last_block
     last_proof = last_block['proof']
-    proof = blockchain.proof_of_work(last_proof, difficulty=5)
+    proof = blockchain.proof_of_work(last_proof)
 
     # We must receive a reward for finding the proof
     # The sender is "0" to signify that this node has mined a new coin
@@ -165,5 +246,44 @@ def full_chain():
     return jsonify(response), 200
 
 
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
+    app.run(host='0.0.0.0', port=port)
